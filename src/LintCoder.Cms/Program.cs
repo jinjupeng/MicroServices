4e5b13f0ff9e111b1555db21ef1de423367f65c4
jinjupeng <im.jp@outlook.com>
2022-11-05 19:50:56 +0800

    优化

--- src/LintCoder.Cms/Program.cs
+++ src/LintCoder.Cms/Program.cs
@@ -1,7 +1,4 @@
 using NLog.Web;
-using Ocelot.DependencyInjection;
-using Ocelot.Middleware;
-using Ocelot.Provider.Consul;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,11 +8,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-//builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
-builder.Services.AddOcelot(builder.Configuration);
-builder.Services.AddSwaggerForOcelot(builder.Configuration);
-builder.Services.AddOcelot(builder.Configuration).AddConsul().AddConfigStoredInConsul();
 
 builder.Logging.ClearProviders();
 builder.Host.UseNLog();
@@ -27,14 +19,9 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    //app.UseSwaggerForOcelotUI(opt => {
-    //    opt.PathToSwaggerGenerator = "/swagger/docs";
-    //});
 }
 
 app.UseHttpsRedirection();
-
-//await app.UseOcelot();
 
 app.UseAuthorization();
 
